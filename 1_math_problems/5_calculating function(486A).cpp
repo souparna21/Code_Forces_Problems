9dafb5a81c072215bcadbfb78e7dd5c312b66e73
Souparna Bhowmik <jsouparna2@gmail.com>
2021-06-28 17:49:42 +0530

    initial commit

---
+++ 1_math_problems/5_calculating function(486A).cpp
@@ -0,0 +1,23 @@
+/*
+For a positive integer n let's define a function f:
+
+f(n) =  - 1 + 2 - 3 + .. + ( - 1)nn
+
+Your task is to calculate f(n) for a given integer n.
+
+Input
+The single line contains the positive integer n (1 ≤ n ≤ 1015).
+
+Output
+Print f(n) in a single line.
+*/
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    long long n;
+    cin >> n;
+    cout << n / 2 - n % 2 * n;
+}
