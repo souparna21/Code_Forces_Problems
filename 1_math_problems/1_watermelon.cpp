f14621be5ccd656a53a2def60bde865bab79c1a8
Souparna Bhowmik <jsouparna2@gmail.com>
2021-06-30 19:54:02 +0530

    final commit

--- 1_math_problems/1_watermelon.cpp
+++ 1_math_problems/1_watermelon.cpp
@@ -8,6 +8,8 @@ The first (and the only) input line contains integer number w (1 ≤ w ≤
 
 Output
 Print YES, if the boys can divide the watermelon into two parts, each of them weighing even number of kilos; and NO in the opposite case.
+
+Write the code now
 */
 #include <iostream>
 main()
